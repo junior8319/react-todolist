d95bebbca16b690653e90cfd7154879bf3134a5e
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-12-14 20:00:30 -0300

    backend development -routes - index: Sets the index for api routes.

---
+++ backend/src/routes/index.routes.ts
@@ -0,0 +1,9 @@
+import usersRouter from './users.routes';
+import tasksRouter from './tasks.routes';
+import loginRouter from './login.routes';
+
+export {
+  usersRouter,
+  tasksRouter,
+  loginRouter,
+};
