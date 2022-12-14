dc98578ffd7bdce689fa882417efd32d1037bfb4
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-12-14 19:37:33 -0300

    frontend development - components - Sets the TodoList component.

---
+++ frontend/src/components/todoList/TodoList.tsx
@@ -0,0 +1,13 @@
+import Content from "./content/Content";
+import Title from "./title/Title";
+
+const TodoList = () => {
+  return (
+    <main>
+      <Title />
+      <Content />
+    </main>
+  );
+};
+
+export default TodoList;
