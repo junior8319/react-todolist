cfef555ce60d96bb46535be0e0366cd4878571dc
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-12-14 18:27:27 -0300

    backend setup - database configuration - migrations - create users: sets the migrations to create the users's table.

---
+++ backend/src/database/migrations/20221204134533-create-users.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      telephone: {
+        type: Sequelize.BIGINT,
+        allowNull: true,
+      },
+
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    });
+  },
+
+  async down (queryInterface, _Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
