83fe7d311134992c80caf0750b1269a627f34eed
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-12-15 11:12:25 -0300

    fixes - frontend - context - types: Add response state to IContext types.

--- frontend/src/interfaces/IContext.ts
+++ frontend/src/interfaces/IContext.ts
@@ -2,11 +2,13 @@ import IUser from '../interfaces/IUser';
 import { ILoginUser } from '../interfaces/ILogin';
 import IToken from '../interfaces/IToken';
 import ITask from '../interfaces/ITask';
+import IError from './IError';
 
 export default interface IContext {
   users: IUser[] | null;
   tasks: ITask[] | [];
   userLogged: IUser | null;
+  response: IError | null;
   token: IToken | null;
   login: ILoginUser;
   isLoginOpen: boolean;
