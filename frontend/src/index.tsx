10dba0489204715a3b8a91c2584fa048481ad8a8
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-12-16 10:54:41 -0300

    frontend - feature - User: Sets route for user page.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
 import LoginComp from './components/login/LoginComp';
+import User from './pages/user/User';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,6 +14,7 @@ root.render(
       <Routes>
         <Route path="/" element={ <App /> } />
         <Route path="/login" element={ <LoginComp /> } />
+        <Route path="/users" element={ <User /> } />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
