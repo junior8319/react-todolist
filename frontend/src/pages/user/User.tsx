e1903eeab2ec08afa7021f019f91725953dec47f
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-12-16 19:57:43 -0300

    frontend - feature - User: Sets the main user page.

--- frontend/src/pages/user/User.tsx
+++ frontend/src/pages/user/User.tsx
@@ -1,15 +1,17 @@
 import { useContext } from 'react';
+import RegisterUser from '../../components/register/RegisterUser';
 import { TodoContext } from '../../context/TodoContext';
 import IContext from '../../interfaces/IContext';
 
 const User = () => {
   const { userLogged } = useContext(TodoContext) as IContext;
+  
   return (
     (userLogged)
     ?
       <h1>Hello, { userLogged?.name }</h1>
     :
-      <h1>Register page</h1>
+      <RegisterUser />
   );
 };
 
