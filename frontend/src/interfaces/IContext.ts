70b6659d0b629ec212190e59eca20ee29d44451b
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-12-16 20:11:23 -0300

    frontend - feature - IContext interface: Adds types for inputs's changes and hangdling register request to user in registration.

--- frontend/src/interfaces/IContext.ts
+++ frontend/src/interfaces/IContext.ts
@@ -1,10 +1,11 @@
-import IUser from '../interfaces/IUser';
+import IUser, { IRegistering } from '../interfaces/IUser';
 import { ILoginUser } from '../interfaces/ILogin';
 import IToken from '../interfaces/IToken';
 import ITask from '../interfaces/ITask';
 import IError from './IError';
 
 export default interface IContext {
+  inRegistrationUser: IRegistering;
   users: IUser[] | null;
   tasks: ITask[] | [];
   userLogged: IUser | null;
@@ -13,8 +14,10 @@ export default interface IContext {
   login: ILoginUser;
   isLoginOpen: boolean;
   handleChange: (event: any) => void;
+  handleRegisterChange: (event: any) => void;
   handleLogin: (event: any) => void;
   handleLogout: (event: any) => void;
+  handleRegister: (event: any) => void;
   openLogin: () => void;
   closeLogin: () => void;
 }
