24d2e4fe75519a3ef3db0ce57c2d124fab820819
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-12-17 08:37:55 -0300

    frontend - feature - User page: Add TopComp as header of the page; Add Register user component to the page.

--- frontend/src/pages/user/User.tsx
+++ frontend/src/pages/user/User.tsx
@@ -2,16 +2,23 @@ import { useContext } from 'react';
 import RegisterUser from '../../components/register/RegisterUser';
 import { TodoContext } from '../../context/TodoContext';
 import IContext from '../../interfaces/IContext';
+import TopComp from '../../components/top/TopComp';
 
 const User = () => {
   const { userLogged } = useContext(TodoContext) as IContext;
   
   return (
-    (userLogged)
-    ?
-      <h1>Hello, { userLogged?.name }</h1>
-    :
-      <RegisterUser />
+    <>
+      <TopComp />
+      {
+        userLogged
+        ?
+          <h1>Hello, {userLogged.name}!</h1>
+        :
+        <RegisterUser />
+      }
+    </>
+    
   );
 };
 
