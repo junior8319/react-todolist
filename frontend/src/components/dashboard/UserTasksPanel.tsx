1937059c5fb86298c31b2dfb84f49f8536914113
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-12-19 15:04:48 -0300

    frontend - feature - Task - User tasks panel: Adds the task form to the User tasks panel; Remove unused context attributes.

--- frontend/src/components/dashboard/UserTasksPanel.tsx
+++ frontend/src/components/dashboard/UserTasksPanel.tsx
@@ -1,14 +1,12 @@
-import { useContext } from 'react';
-import { TodoContext } from '../../context/TodoContext';
-import IContext from '../../interfaces/IContext';
 import styles from './userPanel.module.scss';
 import Todo from '../todoList/content/todo/Todo';
 import Done from '../todoList/content/done/Done';
+import TasksForm from '../forms/Tasks.form';
 
 const UserTasksPanel = () => {
-  const { userLogged } = useContext(TodoContext) as IContext;
   return (
     <main className={ styles['panel-container'] }>
+      <TasksForm />
       <Todo />
       <Done />
     </main>
